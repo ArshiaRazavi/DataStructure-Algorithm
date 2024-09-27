de78aac5929680cabb78146baf85d68007adb9e0
ArshiaRazavi <SeyedArshia.Razavi@ucalgary.ca>
2024-09-26 20:50:55 -0600

    insertion sort algorithm

--- Session1/Insertion-sort.cpp
+++ Session1/Insertion-sort.cpp
@@ -8,16 +8,16 @@ void insertion_sort(int *A, int size){
     if (size == 0) return;
 
     for (int i = 1; i < size; i++){
-        int to_be_sorted = A[i+1];
-        int k = i;
-        for (;A[k] > to_be_sorted; k--)
+        int to_be_sorted = A[i];
+        int k = i-1;
+        for (;A[k] > to_be_sorted && k >= 0; k--)
             A[k + 1] = A[k];
         A[k+1] = to_be_sorted;
     }
 }
 
 int main(){
-    int A[] = {10 , 20, 40, 23, 11};
+    int A[] = {12 , 20, 40, 23, 11};
     int size = sizeof(A)/sizeof(int);
     printf("Initial Array: \n");
     for (int i = 0; i < size; i++)
